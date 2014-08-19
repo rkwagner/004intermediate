eb7793179be2692ab22761cb8f14b458776f9566
Ryan Wagner <rkwagner@ucsd.edu>
2014-08-19 16:13:59 -0700

    Calculator now creates a postfix expression.

--- calculator.c
+++ calculator.c
@@ -5,34 +5,91 @@
 //Idea: use recursion to create a stack every time a paren is used.
 //Valid symbols:  /-+*%^()
 //Priorities:     1.()    2.^   3.*/%   4.+-
-//
+//Use a stack and a postfix string, popping the stack until the item of
+//lower priority is the next on the stack, at which point a push occurs.
+//numbers are always parsed in order and put directly into the string.
 #include<stdio.h>
-#include<string>
+#include<string.h>
+#include<stdlib.h>
 
-int add(char *a);
-int sub(char *a);
-int mult(char *a);
-int div(char *a);
-int mod(char *a);
-int exp(char *a);
-int paren(char *a);
+#define MAX_CMP 4
 
-int main(void){
-  printf("Enter an equation: ");
-  char in[100];
-  fgets(in,sizeof in,stdin);
-  printf("Equation results: %s\n",in);
-  return 0;
+void input(char *a){
+  printf("> ");
+  fgets(a, 256, stdin);
 }
 
-int paren(char *a){
-  char *closeParen=strchr(a,')');
-}//Operate on everything until a close paren is found.
+int priorityCh(char a, char b){
+  int priorityA,priorityB;
+  switch(a){
+    case '+':case '-':priorityA=0;break;
+    case '*':case '/':case '%':priorityA=1;break;
+    case '^':case ')':priorityA=2;break;
+    case '(':priorityA=3;break;
+    default:priorityA=-100;break;
+  }
+  switch(b){
+    case '+':case '-':priorityB=0;break;
+    case '*':case '/':case '%':priorityB=1;break;
+    case '^':case ')':priorityB=2;break;
+    case '(':priorityB=3;break;
+    default:priorityB=-1;break;
+  }
+  return priorityA-priorityB;
+}//Check which symbol is of higher priority.  If b, pop the stack until
+ //a is highest priority left.
 
-int exp(char *a){
-  
-}//Recurse until parsed int is achieved.
+void push(char *a, char b){
+  a[strlen(a)]=b;
+  a[strlen(a)+1]='\0';
+}
 
-int mod(char *a){
+char pop(char *a){
+  char newA = a[strlen(a)-1];
+  a[strlen(a)-1]='\0';
+  return newA;
+}
 
-}//Find the remainder of division by the second int.
+void calculate(char *infix, char *postfix, char *bucket){
+  if(!(*infix) || *infix=='\n'){
+    if(strlen(bucket)){
+      push(postfix,pop(bucket));
+    }
+    else{/*EMPTY*/}
+    (void)printf("%s\n", postfix);
+  }
+  else{
+    if(*infix<='9'&&*infix>='0'){
+      push(postfix,*infix);
+      calculate(infix+1,postfix, bucket);
+    }
+    else{
+      int checker=priorityCh(*infix,bucket[strlen(bucket)-1]);
+      if(checker>0){
+        push(bucket,*infix);
+      }
+      else if(checker<-50){
+        *infix=infix[strlen(infix)-1];
+        (void)printf("Invalid input detected, terminating...\n");
+      }
+      else{
+        while(checker<=0&&checker>=-50 && strlen(bucket)>0){
+          push(postfix,pop(bucket));
+          checker=priorityCh(*infix,bucket[strlen(bucket)-1]);
+        }
+        push(bucket,*infix);
+      }
+      calculate(infix+1,postfix, bucket);
+    }
+  }
+}//Create a postfix expression to evaluate and calculate.
+
+int main(void){
+  char in[256],post[256]="",bucket[256]="";
+  while(strncmp(in,"quit",MAX_CMP)){
+    input(in);
+    strncmp(in,"quit",MAX_CMP)?(calculate(in,post,bucket)):
+      (void)printf("Sequence terminated.\n");
+  }
+  return 0;
+}
