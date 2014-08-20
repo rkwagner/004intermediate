52b24be9dc2313b87ecede9654d81deaefd82c58
Ryan Wagner <rkwagner@ucsd.edu>
2014-08-19 18:36:46 -0700

    Properly evaluates to postfix, next step will be the stack math.

--- calculator.c
+++ calculator.c
@@ -24,16 +24,18 @@ int priorityCh(char a, char b){
   switch(a){
     case '+':case '-':priorityA=0;break;
     case '*':case '/':case '%':priorityA=1;break;
-    case '^':case ')':priorityA=2;break;
-    case '(':priorityA=3;break;
+    case '^':priorityA=2;break;
+    case ')':priorityA=-2;break;
+    case '(':priorityA=-1;break;
     default:priorityA=-100;break;
   }
   switch(b){
     case '+':case '-':priorityB=0;break;
     case '*':case '/':case '%':priorityB=1;break;
-    case '^':case ')':priorityB=2;break;
-    case '(':priorityB=3;break;
-    default:priorityB=-1;break;
+    case '^':priorityB=2;break;
+    case ')':priorityB=-2;break;
+    case '(':priorityB=-1;break;
+    default:priorityB=-3;break;
   }
   return priorityA-priorityB;
 }//Check which symbol is of higher priority.  If b, pop the stack until
@@ -42,26 +44,53 @@ int priorityCh(char a, char b){
 void push(char *a, char b){
   a[strlen(a)]=b;
   a[strlen(a)+1]='\0';
-}
+}//Pushes onto the "stack" (string).
 
 char pop(char *a){
   char newA = a[strlen(a)-1];
   a[strlen(a)-1]='\0';
   return newA;
-}
+}//Pops the "stack" (string).
+
+void evalPost(char *a){
+  (void)printf("%s\n", a);
+  //while not mathematical expression, push onto stack.
+  //when math is reached, pop the last 2 pushed values as rval and lval,
+  //performing math on the resultant expression as specified.
+  //Iterate without deletion by incrementing position of a and calling eval.
+  char lval[1],rval[1],stack[256]="";
+  while (*a!='\0' && *a){
+    if(*a > 47 && *a < 58){
+      push(stack,*a);
+    }
+    else{
+    }
+    a++;
+  }
+}//Performs mathematical ops on postfix expression using a second stack.
 
 void calculate(char *infix, char *postfix, char *bucket){
   if(!(*infix) || *infix=='\n'){
-    if(strlen(bucket)){
+    while(strlen(bucket)){
       push(postfix,pop(bucket));
     }
-    else{/*EMPTY*/}
-    (void)printf("%s\n", postfix);
+    evalPost(postfix);
   }
   else{
     if(*infix<='9'&&*infix>='0'){
       push(postfix,*infix);
       calculate(infix+1,postfix, bucket);
+    }
+    else if(*infix=='('){
+      push(bucket,*infix);
+      calculate(infix+1,postfix,bucket);
+    }
+    else if(*infix==')'){
+      while(bucket[strlen(bucket)-1]!='('&&strlen(bucket)){
+        push(postfix,pop(bucket));
+      }
+      (void)pop(bucket);
+      calculate(infix+1,postfix,bucket);
     }
     else{
       int checker=priorityCh(*infix,bucket[strlen(bucket)-1]);
@@ -85,9 +114,10 @@ void calculate(char *infix, char *postfix, char *bucket){
 }//Create a postfix expression to evaluate and calculate.
 
 int main(void){
-  char in[256],post[256]="",bucket[256]="";
+  char in[256];
   while(strncmp(in,"quit",MAX_CMP)){
     input(in);
+    char post[256]="",bucket[256]="";
     strncmp(in,"quit",MAX_CMP)?(calculate(in,post,bucket)):
       (void)printf("Sequence terminated.\n");
   }
